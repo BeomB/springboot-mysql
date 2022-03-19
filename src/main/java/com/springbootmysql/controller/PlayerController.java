93426b1f081906882baea9f1281c3d430d161045
beombeom1119 <qjarl1119@naver.com>
2022-03-19 17:28:06 +0900

    REST API With Mysql 완료

--- src/main/java/com/springbootmysql/controller/PlayerController.java
+++ src/main/java/com/springbootmysql/controller/PlayerController.java
@@ -6,8 +6,9 @@ import com.springbootmysql.repository.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
-@RequestMapping(value = "/player")
 public class PlayerController {
     private final PlayerRepository playerRepository;
 
@@ -39,6 +40,39 @@ public class PlayerController {
         playerRepository.save(player);
         return "add";
     }
+
+
+    @PutMapping(value = "/update/{number}")
+    public Player Update(@PathVariable Long number,@RequestParam String name, @RequestParam Long goal, Long assist)
+    {
+        Optional<Player> player = playerRepository.findById(number);
+        player.get().setName(name);
+        player.get().setGoal(goal);
+        player.get().setAssist(assist);
+        return playerRepository.save(player.get());
+    }
+
+    @PutMapping(value = "/updated/{number}")
+    public Player Updated(@RequestBody Player toplayer, @PathVariable long number)
+    {
+        Optional<Player> find_player = playerRepository.findById(number);
+        find_player.get().setName(toplayer.getName());
+        find_player.get().setGoal(toplayer.getGoal());
+        find_player.get().setAssist(toplayer.getAssist());
+        return playerRepository.save(find_player.get());
+    }
+
+    @DeleteMapping(value = "delete/{number}")
+    public String Delete(@PathVariable long number)
+    {
+        playerRepository.deleteById(number);
+        return "success";
+    }
+
+
+
+
+
 
 
 }
